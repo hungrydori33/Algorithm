f2aaa215aae4d307be6f49f4cd2e8831e76571ac
민상훈 <108341477+hungrydori33@users.noreply.github.com>
2024-06-25 18:06:14 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -11,3 +11,12 @@ class Solution {
         return answer;
     }
 }
+
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public int solution(String[] s1, String[] s2) {
+//         return (int) Arrays.stream(s1).map(s -> Arrays.stream(s2).collect(Collectors.toList()).contains(s)).filter(b -> b).count();
+//     }
+// }
