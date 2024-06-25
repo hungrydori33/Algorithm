3c171e573e0f0e35654cb4374999edb6f5bc8ad6
민상훈 <108341477+hungrydori33@users.noreply.github.com>
2024-06-25 17:52:52 +0900

    [level 0] Title: 배열 만들기 1, Time: 5.84 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,11 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int n, int k) {
-        int[] answer = new int[n/k];
-        for(int i = 1; i <= n/k; i++){
-            int multi = k*i;
-            answer[i-1] = multi;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
     }
 }
